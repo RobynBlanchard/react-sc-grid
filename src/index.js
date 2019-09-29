8665f2e55cd3bdc791a08daf7444369d563bbd4d
Robyn <robynblanchard@notonthehighstreet.com>
2019-09-29 15:23:58 +0100

    Don't import as default

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-export { default as Column } from './components/Column';
-export { default as Row } from './components/Row';
+import Column from './components/Column';
+import Row from './components/Row';
 
-
-
-// import Component1 from './Component1';
-// import Component2 from './Component2';
-// import Component3 from './Component3';
-
-// export {
-//     Component1,
-//     Component2,
-//     Component3
-// };
+export {
+    Column,
+    Row,
+};
