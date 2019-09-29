2d188c1ff27d31ddec990b01c08e5bb96edb92a3
Robyn <robynblanchard@notonthehighstreet.com>
2019-09-29 15:44:54 +0100

    Add storybook

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const gridWidth = '114rem';
-export const gutterVertical = '8rem';
-export const gutterHorizontal = '6rem';
+export const gridWidth = '100%';
+export const gutterVertical = '8px';
+export const gutterHorizontal = '8px';
