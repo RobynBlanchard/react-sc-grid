20b409cb2c6ef77869aee138772916a378475d34
Robyn <robynblanchard@notonthehighstreet.com>
2019-09-29 15:13:35 +0100

    Set up webpack and babel

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export { default as Test } from './components/Test';
+
+
+
+// import Component1 from './Component1';
+// import Component2 from './Component2';
+// import Component3 from './Component3';
+
+// export {
+//     Component1,
+//     Component2,
+//     Component3
+// };
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js"
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
+    },
+
+    "styled-components": {
+      root: "styled",
+      commonjs2: "styled-components",
+      commonjs: "styled-components",
+      amd: "styled-components",
+      umd: "styled-components"
+    }
+  }
+};
